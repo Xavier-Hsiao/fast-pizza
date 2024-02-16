c0123d7f30b3886d8062a639459a974132cd3083
Xavier-Hsiao <tim88776@gmail.com>
2024-02-16 21:36:34 +0800

    Build: add react router loader to fetch restaurant api in Menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,3 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
 export default function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
   return <h1>Menu</h1>;
 }
